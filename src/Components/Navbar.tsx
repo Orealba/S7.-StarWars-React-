aebffc3f3bb7b6dbf2316a602901e702d7642fd3
Orealba Soriano <orealbas@gmail.com>
2024-12-17 13:46:35 +0100

    ending the navbar

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,24 +1,23 @@
-import "./styles/Navbar.css";
+import './styles/Navbar.css';
 
 export const Navbar = () => {
   return (
     <>
       <div className="fixed top-32 left-1/2 transform -translate-x-1/2 z-50 w-full px-4 border-y border-gray-700">
         <nav className="bg-transparent">
-          <div className="flex justify-center items-center  ">
-            <ul className="flex space-x-8 items-center">
-              <li className="border-x border-gray-700 px-3 py-2">
+          <div className="flex justify-center items-center ml- ">
+            <ul className="flex space-x-1 items-center">
+              <li className="border-x border-gray-700 border-b-2 border-b-transparent px-3 py-2 hover:border-b-blue-500 active:border-b-blue-500">
                 <a
                   href="#"
-                  className="block  text-gray-400 rounded  md:bg-transparent hover:text-white active:text-white hover:font-bold active:font-bold"
-                  aria-current="page">
+                  className="text-gray-400 rounded md:bg-transparent hover:text-white active:text-white hover:font-bold active:font-bold">
                   HOME
                 </a>
               </li>
-              <li className="border-r border-gray-700  py-2 pr-3 starship-button">
+              <li className="border-r border-gray-700 border-b-2 border-b-transparent py-2 px-1 hover:border-b-blue-500 active:border-b-blue-500">
                 <a
                   href="#"
-                  className="block   text-gray-400 rounded md:bg-transparent hover:text-white active:text-white hover:font-bold active:font-bold">
+                  className="text-gray-400 rounded md:bg-transparent hover:text-white active:text-white hover:font-bold active:font-bold">
                   STARSHIPS
                 </a>
               </li>
