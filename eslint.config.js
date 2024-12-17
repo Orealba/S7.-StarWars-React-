44668a3a319a3cd5a331fe2753dd89fc674fe72e
Orealba Soriano <orealbas@gmail.com>
2024-12-17 11:36:11 +0100

    ending the header

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,8 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -18,6 +18,10 @@ export default tseslint.config(
       'react-refresh': reactRefresh,
     },
     rules: {
+      'bem/class-name': [
+        'error',
+        { ignorePatterns: ['^(w-|h-|sm:|md:|lg:|mt-|transform)'] },
+      ],
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
@@ -25,4 +29,4 @@ export default tseslint.config(
       ],
     },
   },
-)
+);
--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,14 +1,29 @@
-import logo from '../assets/images/logo.webp';
+import logo from '../assets/images/logo.png';
 import '../Components/styles/Header.css';
 
 export const Header = () => {
   return (
     <>
-      <img
-        src={logo}
-        alt="logo"
-        className="header-logo"
-      />
+      <div className="grid grid-cols-3 fixed top-0 left-1/2 transform -translate-x-1/2 z-50 w-full px-4">
+        <img
+          src={logo}
+          alt="logo"
+          className="mt-4 w-32 h-14 sm:w-40 sm:h-16 md:w-48 md:h-20 lg:w-[12.5rem] lg:h-[5.8rem] mx-auto col-start-2"
+        />
+        <div className="flex gap-4 items-center justify-end">
+          <button
+            type="button"
+            className="bg-transparent text-white font-medium rounded-lg text-sm px-1  text-center outline-none focus:outline-none hover:outline-none border-none focus:border-none hover:border-none ring-0 focus:ring-0 hover:ring-0 hover:scale-105 transition-transform">
+            LOGIN
+          </button>
+          <p className='text-gray-500'>//</p>
+          <button
+            type="button"
+            className="bg-transparent text-white font-medium rounded-lg text-sm px-3  text-center outline-none focus:outline-none hover:outline-none border-none focus:border-none hover:border-none ring-0 focus:ring-0 hover:ring-0 hover:scale-105 transition-transform">
+            SIGNUP
+          </button>
+        </div>
+      </div>
     </>
   );
 };
