0479a28d7e74470f2cf10267173757a18ad336ea
Orealba Soriano <orealbas@gmail.com>
2024-12-18 16:15:43 +0100

    adding styles in the starships page

--- src/Pages/Starships.tsx
+++ src/Pages/Starships.tsx
@@ -3,23 +3,37 @@ import bgImage from '../assets/images/bg.webp';
 
 export const Starships = () => {
   return (
-    <div 
-      className="min-h-screen w-full bg-center bg-no-repeat bg-fixed"
-      style={{ 
-        backgroundImage: `url(${bgImage})`,
-        backgroundSize: 'contain',
-        backgroundColor: '#111827'
-      }}
-    >
-      <div className="flex flex-col items-start justify-start">
-        <StarshipCard />
-      </div>
-      
-      <button
-        type="button"
-        className="px-12 py-4 mt-20 text-lg font-bold text-white bg-black border-2 border-yellow-400 rounded-full hover:bg-yellow-400 hover:text-black hover:scale-105 transition-all duration-300">
-        VER MÁS
-      </button>
+    <div>
+      <main
+        style={{
+          position: 'fixed',
+          top: 130,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          backgroundImage: `url(${bgImage})`,
+          backgroundSize: 'cover',
+
+          backgroundRepeat: 'no-repeat',
+
+          flexDirection: 'column',
+        }}>
+        <div className="flex justify-start mt-0 ">
+          <h1 className="text-white text-7xl mb-16 font-bold ml-5 mt-20">
+            STARSHIPS
+          </h1>
+        </div>
+
+        <div className="flex-1 flex flex-col mt-12 items-center justify-center gap-8">
+          <StarshipCard />
+
+          <button
+            type="button"
+            className=" px-12 py-4 text-lg font-bold text-white bg-black border-2 border-yellow-400 rounded-full hover:bg-yellow-400 hover:text-black hover:scale-105 transition-all duration-300">
+            VER MÁS
+          </button>
+        </div>
+      </main>
     </div>
   );
 };
